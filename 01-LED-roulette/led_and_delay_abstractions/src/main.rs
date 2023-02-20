4ea4d1b70a05c0897c30f998af398bf6fc5ce96d
petrostrak <pit.trak@gmail.com>
2023-02-20 16:10:50 +0200

    Change led roulette pattern

--- 01-LED-roulette/led_and_delay_abstractions/src/main.rs
+++ 01-LED-roulette/led_and_delay_abstractions/src/main.rs
@@ -8,27 +8,33 @@ use aux5::{entry, Delay, DelayMs, LedArray, OutputSwitch};
 fn main() -> ! {
     let (mut delay, mut leds): (Delay, LedArray) = aux5::init();
 
-    let half_period = 500_u16;
+    let ping_period = 100_u16;
 
     loop {
         leds[0].on().ok();
-        leds[1].off().ok();
+        delay.delay_ms(ping_period);
+        leds[1].on().ok();
+        delay.delay_ms(ping_period);
         leds[2].on().ok();
-        leds[3].off().ok();
+        delay.delay_ms(ping_period);
+        leds[3].on().ok();
+        delay.delay_ms(ping_period);
         leds[4].on().ok();
-        leds[5].off().ok();
+        delay.delay_ms(ping_period);
+        leds[5].on().ok();
+        delay.delay_ms(ping_period);
         leds[6].on().ok();
-        leds[7].off().ok();
-        delay.delay_ms(half_period);
+        delay.delay_ms(ping_period);
+        leds[7].on().ok();
+        delay.delay_ms(ping_period);
 
         leds[0].off().ok();
-        leds[1].on().ok();
+        leds[1].off().ok();
         leds[2].off().ok();
-        leds[3].on().ok();
+        leds[3].off().ok();
         leds[4].off().ok();
-        leds[5].on().ok();
+        leds[5].off().ok();
         leds[6].off().ok();
-        leds[7].on().ok();
-        delay.delay_ms(half_period);
+        leds[7].off().ok();
     }
 }
